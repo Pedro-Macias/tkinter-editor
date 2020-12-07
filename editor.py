ebf2133e2e4b632ff1b4fd5facaaa00c54c7c383
Pedro Macias <pin.macias@gmail.com>
2020-12-07 13:32:23 +0100

    eleccion de fuente

--- editor.py
+++ editor.py
@@ -74,6 +74,20 @@ def color_fondo():
     color_bg= color[1]
     texto.config(bg='{}'.format(color_bg))
     
+# FUENTES
+def arial():
+    fuente.set('Arial')
+    texto.config(font=('Arial',12))
+def calibri():
+    fuente.set('Calibri')
+    texto.config(font=('Calibri',12))
+def lucida():
+    fuente.set('Lucida Console')
+    texto.config(font=('Lucida Console',12))
+def comic():
+    fuente.set('Comic Sans MS')
+    texto.config(font=('Comic Sans MS',12))
+
 
 # CREAR LA RAIZ
 
@@ -97,10 +111,15 @@ formatmenu = Menu(menubar,tearoff=0)
 formatmenu.add_command(label='Color Texto', command=color_texto)
 formatmenu.add_command(label ='Color Fondo',command=color_fondo)
 
+fuentemenu = Menu(menubar,tearoff=0)
+fuentemenu.add_command(label='Arial',command=arial)
+fuentemenu.add_command(label='Calibri',command=calibri)
+fuentemenu.add_command(label='Lucida Console',command = lucida)
+fuentemenu.add_command(label='Comic Sans MS',command= comic)
 
 menubar.add_cascade(menu=filemenu, label='Archivo')
 menubar.add_cascade(menu=formatmenu, label='Color')
-
+menubar.add_cascade(menu=fuentemenu,label='Fuente')
 
 
 root.config(menu=menubar)
@@ -112,12 +131,15 @@ texto.config(bd=0,padx=6, pady=4, font=('Comic-sand',12))
 
 # monitor inferior
 mensaje= StringVar()
+fuente= StringVar()
 mensaje.set('Bienvenido')
 monitor =Label(root,textvar=mensaje, justify='left')
 monitor.pack(side='left')
 color_editor = Label(root,text='Color Texto', justify= 'center')
-color_editor.pack(side='bottom')
-
+color_editor.pack(side='right',padx=20)
+fuente.set('Arial')
+tipo_fuente=Label(root,textvar=fuente, justify='right')
+tipo_fuente.pack(side='right')
 
 
 # SIEMPRE TIENE QUE IR ABAJO . 
